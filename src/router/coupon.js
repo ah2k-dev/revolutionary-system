fce8d5296878858051133b5f7a76f8dec1de01dc
zubair arif <zubarif234@gmail.com>
2023-08-22 23:31:15 +0500

    refactor coupon api

---
+++ src/router/coupon.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const coupon = require("../controllers/couponController");
+const isAuthenticated = require("../middleware/auth");
+
+router.route("/createCoupon").post(isAuthenticated, coupon.createCoupon);
+
+module.exports = router;
