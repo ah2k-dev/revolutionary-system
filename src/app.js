929833691850581f0ae431fabcb910dcf831d670
Umer Memon <117743102+umermemon4648@users.noreply.github.com>
2023-08-29 11:19:07 +0500

    Update app.js

--- src/app.js
+++ src/app.js
@@ -26,20 +26,21 @@ app.use(
   })
 ), // 1 MB
   app.use(
-<<<<<<< Updated upstream
-    "/rev-be/uploads",
-=======
     // "/rev-be/uploads",
     "/uploads",
->>>>>>> Stashed changes
     express.static(path.join(__dirname, "../uploads"))
   );
 // router index
-app.use("/rev-be", router);
+// app.use("/rev-be", router);
+app.use("/", router);
 // api doc
-app.use("/rev-be/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+// app.use("/rev-be/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
-app.get("/rev-be", (req, res) => {
+// app.get("/rev-be", (req, res) => {
+//   res.send("BE-boilerplate v1.1");
+// });
+app.get("/", (req, res) => {
   res.send("BE-boilerplate v1.1");
 });
 
