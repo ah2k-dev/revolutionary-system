7cc9374118dccbfc7f743f0bfc61b9ed6eb86c56
Ahmed Hassan <ah2k.dev@gmail.com>
2023-08-31 00:33:26 +0500

    minor fix

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -144,7 +144,7 @@ const forgotPassword = async (req, res) => {
     user.passwordResetToken = passwordResetToken;
     user.passwordResetTokenExpires = passwordResetTokenExpires;
     await user.save();
-    const message = `Your password reset token is ${resetPasswordToken} and it expires in 10 minutes`;
+    const message = `Your password reset token is ${passwordResetToken} and it expires in 10 minutes`;
     const subject = `Password reset token`;
     await sendMail(email, subject, message);
     return SuccessHandler(`Password reset token sent to ${email}`, 200, res);
