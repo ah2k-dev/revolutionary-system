1e4088fef537e7999b3a7617440b3b5da5be8ed3
Umer Memon <117743102+umermemon4648@users.noreply.github.com>
2023-09-16 19:28:26 +0500

    created product schema

---
+++ src/models/Supplier/products.ts
@@ -0,0 +1,68 @@
+import mongoose, { Schema } from "mongoose";
+import dotenv from "dotenv";
+dotenv.config({ path: ".././src/config/config.env" });
+import {ProductDocument} from '../../types/models/Supplier/products.types'
+
+
+const productSchema = new Schema<ProductDocument>({
+    supplier: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    brand: {
+      type: String,
+      required: true,
+    },
+    images: {
+      type: [String],
+      required: true,
+      // default:"https://img.freepik.com/free-photo/chicken-skewers-with-slices-sweet-peppers-dill_2829-18813.jpg?size=626&ext=jpg"
+    },
+    slug: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      default: 1,
+    },
+    availableQuantity: {
+      type: Number,
+      default: 0,
+    },
+    itemWeight: {
+      type: Number,
+      default: 0,
+    },
+    size: {
+      type: [Number],
+    },
+    sku: {
+      type: String,
+      default: '',
+    },
+    ratings: {
+      type: Number,
+      default: 0,
+    },
+
+});
+
+
+const products = mongoose.model<ProductDocument>("Product", productSchema);
+
+export default products;
---
+++ src/types/models/Supplier/products.types.ts
@@ -0,0 +1,18 @@
+import mongoose, { Document } from "mongoose";
+
+export interface ProductDocument extends Document {
+  supplier: mongoose.Schema.Types.ObjectId;
+  title: string;
+  desc: string;
+  brand: string;
+  images: string[];
+  slug: string;
+  category: string;
+  price: number;
+  quantity: number;
+  availableQuantity: number;
+  itemWeight: number;
+  size: number[];
+  sku: string;
+  ratings: number;
+  }
